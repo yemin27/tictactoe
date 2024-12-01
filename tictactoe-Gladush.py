ebe27ef9bb11da830064d5f8d6828c942f9c4b7e
yemin27 <isor2010@gmail.com>
2024-12-01 14:48:15 +0100

    tictactoe-Gladush.py

--- tictactoe-Gladush.py
+++ tictactoe-Gladush.py
@@ -1,6 +1,4 @@
-Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> """
+... """
 ... projekt_2.py: druhý projekt do Engeto Online Python Akademie
 ... author: Yegor Gladush
 ... email: ygladush@seznam.cz
